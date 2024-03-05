3629c406487e377c9fcf739de0e33c797acfdb9c
cl-anderson <misslael@gmail.com>
2024-03-05 17:54:57 -0600

    assn05.01 boost trouble update

--- assignment 05/assn05.01/assn05.01.cpp
+++ assignment 05/assn05.01/assn05.01.cpp
@@ -2,15 +2,28 @@
 //
 
 #include <iostream>
+#include <utility>
 #include <vector>
-#include <map>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/config.hpp>
+#include <boost/type_traits/is_function.hpp>
 
 int main()
 {
+	using namespace boost;
+	struct vertex { std::string currency };
+	struct edge { double exchangeRate };
+	using graph_t = adjacency_list<listS, vecS, undirectedS, vertex, edge >;
+	using vertex_t = graph_traits<graph_t>::vertex_descriptor;
+	using edge_t = graph_traits<graph_t>::edge_descriptor;
+	
+	boost::adjacency_list<std::string> exchangeGraph;
+	vertex_t t = boost::add_vertex(vertex{"USD"}, exchangeGraph);
+	exchangeGraph.add_vertex("EUR");
+	exchangeGraph.add_vertex("GBP");
+	exchangeGraph.add_vertex("CAD");
+	exchangeGraph.add_vertex("JPY");
 
-    double exchangeVec[5][5] = { 1.00, 0.92, 0.78, 1.35, 150.35,
-                                 1.08, 1.00, 0.85, 1.47, 163.15,
-                                 1.26, 1.16, 1.00, 1.71, 190.62,
-                                 0.73, 0.67, 0.58, 1.00, 110.18,
-                                 0.0066, 0.0061, 0.0052, 0.0090, 1.00 };
+	exchangeGraph.add_edge()
 }
