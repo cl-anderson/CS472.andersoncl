649141d4f3fd274e6d042e92c5691f1c537ae7fa
cl-anderson <misslael@gmail.com>
2024-03-05 15:26:35 -0600

    assn01 update

---
+++ assignment 05/assn05.01/assn05.01.cpp
@@ -0,0 +1,16 @@
+// assn05.01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+int main()
+{
+
+    double exchangeVec[5][5] = { 1.00, 0.92, 0.78, 1.35, 150.35,
+                                 1.08, 1.00, 0.85, 1.47, 163.15,
+                                 1.26, 1.16, 1.00, 1.71, 190.62,
+                                 0.73, 0.67, 0.58, 1.00, 110.18,
+                                 0.0066, 0.0061, 0.0052, 0.0090, 1.00 };
+}
