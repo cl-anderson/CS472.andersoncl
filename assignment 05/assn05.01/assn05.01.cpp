e6a05fc7984d10e20cf26e5d1b808f67c1234071
cl-anderson <misslael@gmail.com>
2024-03-06 18:38:02 -0600

    assn05.03 and 01 updates

--- assignment 05/assn05.01/assn05.01.cpp
+++ assignment 05/assn05.01/assn05.01.cpp
@@ -83,8 +83,10 @@ void printMST(int parent[], int graph[V][V])
     std::cout << "\nPath or arbitrage conversion, represented as a *maximum* search tree.\n";
     std::cout << "Conversion\n";
     for (int i = 1; i < V; i++)
+    {
         std::cout << parent[i] << " : " << i << " \t"
-        << graph[i][parent[i]] << " \n";
+            << graph[i][parent[i]] << " \n";
+    }
 }
 
 // Function to construct and print MST for
@@ -102,8 +104,7 @@ void primMST(int graph[V][V])
     bool mstSet[V];
 
     // Initialize all keys as INFINITE
-    for (int i = 0; i < V; i++)
-        key[i] = INT_MAX, mstSet[i] = false;
+    for (int i = 0; i < V; i++) { key[i] = INT_MAX, mstSet[i] = false; }
 
     // Always include first 1st vertex in MST.
     // Make key 0 so that this vertex is picked as first
@@ -114,7 +115,8 @@ void primMST(int graph[V][V])
     parent[0] = -1;
 
     // The MST will have V vertices
-    for (int count = 0; count < V - 1; count++) {
+    for (int count = 0; count < V - 1; count++) 
+    {
 
         // Pick the minimum key vertex from the
         // set of vertices not yet included in MST
@@ -128,7 +130,7 @@ void primMST(int graph[V][V])
         // Consider only those vertices which are not
         // yet included in MST
         for (int v = 0; v < V; v++)
-
+        {
             // graph[u][v] is non zero only for adjacent
             // vertices of m mstSet[v] is false for vertices
             // not yet included in MST Update the key only
@@ -136,6 +138,7 @@ void primMST(int graph[V][V])
             if (graph[u][v] && mstSet[v] == false
                 && graph[u][v] < key[v])
                 parent[v] = u, key[v] = graph[u][v];
+        }
     }
 
     // Print the constructed MST
--- assignment 05/assn05.03/assn05.03.cpp
+++ assignment 05/assn05.03/assn05.03.cpp
@@ -44,3 +44,62 @@ boost::adjacency_list<T> customPrim(adjacency_list<T> graph)
     adjacency_list<T> mstTree;
     vertex_t start = graph
 }
+
+#define V 5
+int minKey(int key[], bool mstSet[])
+{
+    // Initialize min value
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < V; v++)
+        if (mstSet[v] == false && key[v] < min)
+            min = key[v], min_index = v;
+
+    return min_index;
+}
+
+// A utility function to print the
+// constructed MST stored in parent[]
+void printMST(int parent[], int graph[V][V])
+{
+    std::cout << "Edge Weight\n";
+    for (int i = 1; i < V; i++)
+    {
+        std::cout << parent[i] << " : " << i << " \t"
+            << graph[i][parent[i]] << " \n";
+    }
+}
+
+// Function to construct and print MST for
+// a graph represented using adjacency
+// matrix representation
+void primMST(int graph[V][V])
+{
+    // Array to store constructed MST
+    int parent[V];
+
+    // Key values used to pick minimum weight edge in cut
+    int key[V];
+
+    // To represent set of vertices included in MST
+    bool mstSet[V];
+
+    // Initialize all keys as INFINITE
+    for (int i = 0; i < V; i++) { key[i] = INT_MAX, mstSet[i] = false; }
+    key[0] = 0;
+
+    parent[0] = -1;
+
+    for (int count = 0; count < V - 1; count++) 
+    {
+        int u = minKey(key, mstSet);
+        mstSet[u] = true;
+        for (int v = 0; v < V; v++)
+        {
+            if (graph[u][v] && mstSet[v] == false
+                && graph[u][v] < key[v])
+                parent[v] = u, key[v] = graph[u][v];
+        }
+    }
+    printMST(parent, graph);
+}
