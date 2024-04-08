d3655caa1559fca4b850894c2e983002a314cd72
cl-anderson <misslael@gmail.com>
2024-04-08 14:10:13 -0500

    07.02 complete

--- assignment 07/assn07.02/assn07.02.cpp
+++ assignment 07/assn07.02/assn07.02.cpp
@@ -1,17 +1,17 @@
 // assn07.02.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // author: cl-anderson
-// sources: https://www.geeksforgeeks.org/convert-string-to-int-in-cpp/
+// sources: https://www.geeksforgeeks.org/convert-string-to-int-in-cpp/,with, https://www.geeksforgeeks.org/sudoku-backtracking-7/
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <typeinfo>
 
-void printBoard(int(&arr)[9][9]);
+void printBoard(int (&arr)[9][9]);
 bool checkRow(int(&arr)[9][9], int num, int row);
 bool checkCol(int(&arr)[9][9], int num, int col);
 bool checkBox(int(&arr)[9][9], int num, int row, int col);
-bool findEmpty(int(&arr)[9][9], int& row, int& col);
+std::pair<int,int> findEmpty(int(&arr)[9][9]);
 bool solver(int(&arr)[9][9]);
 int main()
 {
@@ -40,8 +40,15 @@ int main()
         }
     }
 
-    std::cout << "printing the sudoku board: \n\n";
+    std::cout << "Printing the unsolved sudoku board...\n\n";
     printBoard(board);
+
+    if (solver(board) == true)
+    {
+        std::cout << "Printing solved sudoku board...\n\n";
+        printBoard(board);
+    } 
+    else { std::cout << "Failed to solve board.\n"; }
 
     return 0;
 }
@@ -60,89 +67,87 @@ void printBoard(int(&arr)[9][9])
 
 bool checkRow(int(&arr)[9][9], int num, int row)
 {
-    bool numFree = true;
-    for (int i = 0; i < 9; i++)
+    for (int col = 0; col < 9; col++)
     {
-        if (arr[row][i] == num) { return false; }
+        if (arr[row][col] == num) { return true; }
     }
-    return true;
+    return false;
 }
 
 bool checkCol(int(&arr)[9][9], int num, int col)
 {
-    bool numFree = true;
-    for (int i = 0; i < 9; i++)
+    for (int row = 0; row < 9; row++)
     {
-        if (arr[i][col] == num) { return false; }
+        if (arr[row][col] == num) { return true; }
     }
-    return true;
+    return false;
 }
 
-bool checkBox(int(&arr)[9][9], int num, int row, int col)
+bool checkBox(int(&arr)[9][9], int num, int rowstart, int colstart)
 {
-    if (row < 3 && col < 3)
-    {
-        if (arr[row][col] = num) { return false; }
-    }
-    if (row < 3 && 2 < col < 6)
-    {
-        if (arr[row][col] = num) { return false; }
-    }
-    if (row < 3 && 5 < col < 9)
-    {
-        if (arr[row][col] = num) { return false; }
-    }
+    for (int row = 0; row < 3; row++)
+        for (int col = 0; col < 3; col++)
+            if (arr[row + rowstart][col + colstart] == num) { return true; }
 
-    if (2 < row < 6 && col < 3)
-    {
-        if (arr[row][col] = num) { return false; }
-    }
-    if (2 < row < 6 && 2 < col < 6)
-    {
-        if (arr[row][col] = num) { return false; }
-    }
-    if (2 < row < 6 && 5 < col < 9)
-    {
-        if (arr[row][col] = num) { return false; }
-    }
-
-    if (5 < row < 8 && col < 3)
-    {
-        if (arr[row][col] = num) { return false; }
-    }
-    if (5 < row < 8 && 2 < col < 6)
-    {
-        if (arr[row][col] = num) { return false; }
-    }
-    if (5 < row < 8 && 5 < col < 9)
-    {
-        if (arr[row][col] = num) { return false; }
-    }
-    return true;
+    return false;
 }
 
 bool checkNum(int(&arr)[9][9], int num, int row, int col)
 {
-    if (checkRow(arr, num, row) == true && checkCol(arr, num, col) == true && checkBox(arr, num, row, col) == true)
-    {
-        return true;
-    }
+    return (!checkRow(arr, num, row) && !checkCol(arr, num, col) && !checkBox(arr, num, row - row % 3, col - col % 3) && arr[row][col] == 0);
 }
 
-bool findEmpty(int(&arr)[9][9], int& row, int& col)
+std::pair<int, int> findEmpty(int(&arr)[9][9])
 {
+    std::pair<int, int> location;
+    location.first = -1;
+    location.second = -1;
     for (int row = 0; row < 9; row++)
     {
         for (int col = 0; col < 9; col++)
         {
-            if (arr[row][col] == 0) { return true; }
+            if (arr[row][col] == 0)
+            {
+                location.first = row;
+                location.second = col;
+            }
         } 
     }
-    return false;
+    return location;
 }
 bool solver(int(&arr)[9][9])
 {
-    int row, col;
+    int row = 0;
+    int col = 0;
+    std::pair<int, int> emptypair;
+    emptypair.first = -1;
+    emptypair.second = -1;
+    bool solved = false;
 
-    if (!findEmpty(arr, row, col)) { return true; }
+    std::pair<int, int> emptySpot = findEmpty(arr);
+
+    /*for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {*/
+            if (findEmpty(arr) == emptypair) { return true; }
+            else
+            {
+                std::cout << "Found empty location: " << emptySpot.first << " " << emptySpot.second << std::endl;
+
+                for (int num = 1; num < 10; num++)
+                {
+                    if (checkNum(arr, num, emptySpot.first, emptySpot.second))
+                    {
+                        arr[emptySpot.first][emptySpot.second] = num;
+                        if (solver(arr))
+                            return true;
+
+                        arr[emptySpot.first][emptySpot.second] = 0;
+                    }
+                }
+            }
+     /*   }
+    }*/
+    return solved;
 }
